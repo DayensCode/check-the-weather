ffd5c740d5fab7bcc13eaa5b60a7a5cb3b86f320
Nazarova Diana <Dnazarova@korusconsulting.ru>
2024-07-12 15:50:41 +0300

    feat: deployment

--- src/services/weatherService.js
+++
@@ -1,9 +0,0 @@
-import baseService from './baseService';
-
-export const getWeather = async (city) => {
-    const access_token = "3e350e6959b24f798a1105030240407";
-    const url = `/forecast.json?q=${city}&days=1&key=${access_token}`;
-
-    const result = await baseService.get(url);
-    return result.data;
-}
